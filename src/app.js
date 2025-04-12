ff644dcfa90ef81452d8366020b152a730b3c9eb
khanhttq2511 <khanhttq2511@gmail.com>
2025-04-13 02:27:56 +0700

    fix cors

--- src/app.js
+++ src/app.js
@@ -17,13 +17,7 @@ const historyRoutes = require('./routes/history.routes.js');
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
-
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json()); // Nếu dùng body-parser
